bda38b9819c204b6aa52529a5d56db534e9b855d
Oghenekevwe Faith Agi <faithagi1989@gmail.com>
2023-07-16 18:44:43 -0700

    Alphabets in lowerCase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Print Alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int lowerCase = 'a';
+	while (lowerCase <= 'z')
+	{
+	putchar(lowerCase);
+	lowerCase += 1;
+	}
+	putchar('\n');
+	return (0);
+}
