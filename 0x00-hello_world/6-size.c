b5b6f203b0ca08fdab6bac141cefc6317bd090a8
Oghenekevwe Faith Agi <faithagi1989@gmail.com>
2023-07-17 05:00:54 -0700

    make size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,8 +12,10 @@ int main(void)
 	char c;
 	/*sizeof evaluates  size of a variable*/
 	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("The size of a double is: %lu.\n", (unsigned long)sizeof(d));
-		printf("The aize of a float is: %lu.\n", (unsigned long)sizeof(f));
+		printf("The size of a float is: %lu.\n", (unsigned long)sizeof(f));
 	printf("The size of a char is: %lu.\n", (unsigned long)sizeof(c));
 	return (0);
 }
