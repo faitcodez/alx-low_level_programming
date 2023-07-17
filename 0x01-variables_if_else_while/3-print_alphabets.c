3df9b34c060083a274a4385440e1fc238f407b4e
Oghenekevwe Faith Agi <faithagi1989@gmail.com>
2023-07-16 19:20:47 -0700

    print both lower and upper Case letter

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Print alphabets in lowerCase and upperCase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int lowerCase = 'a';
+	int upperCase = 'A';
+	/*your code here*/
+	while (lowerCase <= 'z')
+	{
+		putchar(lowerCase);
+		lowerCase +=;
+	}
+	while (upperCase <= 'Z')
+	{
+		putchar(upperCase);
+		upperCase += 1;
+	}
+	putchar('\n');
+	return (0);
+}
