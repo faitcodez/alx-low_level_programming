fc5134e392dca58768f852541a6c44ec19f84674
Oghenekevwe Faith Agi <faithagi1989@gmail.com>
2023-07-28 07:40:17 -0700

    hello world

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * _putchar - write the unsigned char c to stdout.
+ *
+ * File: main.h - prototypes of all functions.
+ *
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0x06-pointers_arrays_strings directory.
+ *
+ *
+ * @c: character to be printed.
+ * Return: 0 Always (Success)
+ */
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+#endif
