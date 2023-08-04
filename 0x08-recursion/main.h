d99eb4da40ac12856f6cc611edb97e8d7bf81a2e
Oghenekevwe Faith Agi <faithagi1989@gmail.com>
2023-08-03 22:29:15 -0700

    late work~

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * _putchar - write the unsigned char c to stdout.
+ * File: main.h
+ *
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0x07-recursion directory.
+ * @c: the character to print
+ * Return: 0 Always (Success)
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+int is_palindrome(char *s);
+
+#endif
